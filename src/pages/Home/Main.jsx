0db2da579b20206b66f057b29b0790b413aeb43a
silvamaarcus <devsilvamarcus@gmail.com>
2024-06-20 17:12:24 -0300

    Add Main, Skills...

---
+++ src/pages/Home/Main.jsx
@@ -0,0 +1,89 @@
+const Main = () => {
+  return (
+    <>
+      <main className="container p-0">
+        <h1>Projetos</h1>
+        <div className="flex mt-6">
+          <div className="grid-6">
+            <div>
+              <img src="" alt="Site de casamento" />
+              <div className="mt-3">
+                <h5 className="bold">Site Casamento</h5>
+                <div className="flex gap-3">
+                  <p className="p1">HTML</p>
+                  <p className="p1">SASS/CSS</p>
+                  <p className="p1">JS</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <img src="" alt="Site de casamento" />
+              <div className="mt-3">
+                <h5 className="bold">Site Casamento</h5>
+                <div className="flex gap-3">
+                  <p className="p1">HTML</p>
+                  <p className="p1">SASS/CSS</p>
+                  <p className="p1">JS</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <img src="" alt="Site de casamento" />
+              <div className="mt-3">
+                <h5 className="bold">Site Casamento</h5>
+                <div className="flex gap-3">
+                  <p className="p1">HTML</p>
+                  <p className="p1">SASS/CSS</p>
+                  <p className="p1">JS</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+          <div className="grid-6">
+            <div>
+              <img src="" alt="Site de casamento" />
+              <div className="mt-3">
+                <h5 className="bold">Site Casamento</h5>
+                <div className="flex gap-3">
+                  <p className="p1">HTML</p>
+                  <p className="p1">SASS/CSS</p>
+                  <p className="p1">JS</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <img src="" alt="Site de casamento" />
+              <div className="mt-3">
+                <h5 className="bold">Site Casamento</h5>
+                <div className="flex gap-3">
+                  <p className="p1">HTML</p>
+                  <p className="p1">SASS/CSS</p>
+                  <p className="p1">JS</p>
+                </div>
+              </div>
+            </div>
+
+            <div className="mt-6">
+              <img src="" alt="Site de casamento" />
+              <div className="mt-3">
+                <h5 className="bold">Site Casamento</h5>
+                <div className="flex gap-3">
+                  <p className="p1">HTML</p>
+                  <p className="p1">SASS/CSS</p>
+                  <p className="p1">JS</p>
+                </div>
+              </div>
+            </div>
+
+          </div>
+
+        </div>
+      </main>
+    </>
+  );
+};
+export default Main;

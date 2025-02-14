d02e141c6a4a1b50f6887f70b1c1767eff44f22f
Marcus <devsilvamarcus@gmail.com>
2025-02-14 02:05:47 -0300

    feat: ajustando caminho das imagens

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,3 +1,8 @@
+// Images
+import Project_1_img from "/img/project_1.png";
+import Project_2_img from "/img/project_2.png";
+import Project_3_img from "/img/project_3.png";
+
 const Projects = () => {
   return (
     <>
@@ -14,20 +19,20 @@ const Projects = () => {
           {/* Project 1 */}
           <div className="col-span-12 sm:col-span-4">
             <a href="https://forest-page-pi.vercel.app/" target="_blank">
-              <div className="border-soft-black group relative flex cursor-pointer flex-col items-center justify-center overflow-hidden rounded-md border-4 bg-white/1 text-center backdrop-blur-md hover:border-primary transition-all duration-700">
+              <div className="border-soft-black group hover:border-primary relative flex cursor-pointer flex-col items-center justify-center overflow-hidden rounded-md border-4 bg-white/1 text-center backdrop-blur-md transition-all duration-700">
                 <img
-                  src="/public/img/project_1.png"
+                  src={Project_1_img}
                   alt="Forest"
                   className="absolute inset-0 h-full w-full object-cover opacity-10 blur-sm transition duration-700 group-hover:scale-110 group-hover:opacity-100"
                 />
                 <div className="relative z-10 space-y-4 px-10 py-8">
                   <p className="text-2xl">Forest</p>
                   <img
-                    src="/public/img/project_1.png"
+                    src={Project_1_img}
                     alt="Forest"
                     className="rounded-lg transition duration-700 group-hover:scale-105"
                   />
-                  <div className="flex flex-wrap md:flex-nowrap justify-center gap-4">
+                  <div className="flex flex-wrap justify-center gap-4 md:flex-nowrap">
                     <img
                       src="https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB"
                       alt="React"
@@ -50,16 +55,16 @@ const Projects = () => {
           {/* Project 2 */}
           <div className="col-span-12 sm:col-span-4">
             <a href="https://wedding-ms-psi.vercel.app/" target="_blank">
-              <div className="border-soft-black group relative flex cursor-pointer flex-col items-center justify-center overflow-hidden rounded-md border-4 bg-white/1 text-center backdrop-blur-md hover:border-primary transition-all duration-700">
+              <div className="border-soft-black group hover:border-primary relative flex cursor-pointer flex-col items-center justify-center overflow-hidden rounded-md border-4 bg-white/1 text-center backdrop-blur-md transition-all duration-700">
                 <img
-                  src="/public/img/project_2.png"
+                  src={Project_2_img}
                   alt="Site de Casamento"
                   className="absolute inset-0 h-full w-full object-cover opacity-10 blur-sm transition duration-700 group-hover:scale-110 group-hover:opacity-100"
                 />
                 <div className="relative z-10 space-y-4 px-10 py-8">
                   <p className="text-2xl">Site de Casamento</p>
                   <img
-                    src="/public/img/project_2.png"
+                    src={Project_2_img}
                     alt="Site de Casamento"
                     className="rounded-lg transition duration-700 group-hover:scale-105"
                   />
@@ -86,16 +91,16 @@ const Projects = () => {
           {/* Project 3 */}
           <div className="col-span-12 sm:col-span-4">
             <a href="https://portifolio-bikcraft.vercel.app/" target="_blank">
-              <div className="border-soft-black group relative flex cursor-pointer flex-col items-center justify-center overflow-hidden rounded-md border-4 bg-white/1 text-center backdrop-blur-md hover:border-primary transition-all duration-700">
+              <div className="border-soft-black group hover:border-primary relative flex cursor-pointer flex-col items-center justify-center overflow-hidden rounded-md border-4 bg-white/1 text-center backdrop-blur-md transition-all duration-700">
                 <img
-                  src="/public/img/project_3.png"
+                  src={Project_3_img}
                   alt="Bikcraft"
                   className="absolute inset-0 h-full w-full object-cover opacity-10 blur-sm transition duration-700 group-hover:scale-110 group-hover:opacity-100"
                 />
                 <div className="relative z-10 space-y-4 px-10 py-8">
                   <p className="text-2xl">Bikcraft</p>
                   <img
-                    src="/public/img/project_3.png"
+                    src={Project_3_img}
                     alt="Bikcraft"
                     className="rounded-lg transition duration-700 group-hover:scale-105"
                   />

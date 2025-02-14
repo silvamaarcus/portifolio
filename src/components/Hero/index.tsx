d1221ce8d8ee54360d4edcf16be2041acaa71efb
Marcus <devsilvamarcus@gmail.com>
2025-02-14 02:23:29 -0300

    feat: ajustando botoes do Hero

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -2,6 +2,10 @@ import { ArrowDown } from "akar-icons";
 import Button from "../ui/Button";
 
 const Hero = () => {
+  const downloadResume = () => {
+    window.open("/pdf/marcus-silva-cv.pdf", "_blank");
+  };
+
   return (
     <>
       <section className="custom-containe relative flex h-[600px] flex-col items-center justify-center md:min-h-screen">
@@ -10,20 +14,26 @@ const Hero = () => {
             Prazer em te conhecer! <br /> Eu sou{" "}
             <span className="border-primary border-b-4">Marcus Silva</span>.
           </h1>
-          <p className="my-10 text-center w-[70%] md:text-2xl">
+          <p className="my-10 w-[70%] text-center md:text-2xl">
             Um desenvolvedor front-end apaixonado por criar interfaces e dar
             vida a inovações 👋.
           </p>
-          <div className="flex justify-center gap-4 sm:justify-start">
-            <Button>Download CV</Button>
-            <Button variant="secondary">Contato</Button>
+          <div className="flex items-center justify-center gap-4 sm:justify-start">
+            <Button onClick={downloadResume}>Download CV</Button>
+            <a
+              href="https://wa.me/5531997003074"
+              target="_blank"
+              className="border-primary rounded-md border-2 px-4 py-2 text-white hover:opacity-90"
+            >
+              Contato
+            </a>
           </div>
         </div>
         <button
           onClick={() =>
             window.scrollTo({ top: window.innerHeight, behavior: "smooth" })
           }
-          className="bg-secondary absolute bottom-8 animate-bounce cursor-pointer rounded-full mb-4 p-3 text-black hover:opacity-70"
+          className="bg-secondary absolute bottom-8 mb-4 animate-bounce cursor-pointer rounded-full p-3 text-black hover:opacity-70"
         >
           <ArrowDown strokeWidth={2} size={24} />
         </button>
